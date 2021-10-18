c74fb7712e928a7a1864184c710dfde5e2e25ebb
Galina Mamaeva <orang811@icloud.com>
2021-10-18 17:23:40 +0300

    36,1

---
+++ JavaAppium/QAJA_MOBILE/src/test/java/tests/SearchTest.java
@@ -0,0 +1,54 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.ui.MainPageObject;
+import lib.ui.pages.factory.OnboardingPageFactory;
+import lib.ui.pages.factory.SearchPageFactory;
+import lib.ui.pages.factory.StartPageFactory;
+import lib.ui.pages.interfaces.IOnboardingPageObject;
+import lib.ui.pages.interfaces.ISearchPageObject;
+import lib.ui.pages.interfaces.IStartPageObject;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import java.lang.reflect.MalformedParameterizedTypeException;
+
+public class SearchTest extends CoreTestCase {
+
+    @Test
+    public void testFindJava() {
+
+        //org.wikipedia:id/search_container
+
+        //org.wikipedia:id/search_bar
+        MainPageObject mainPO = new MainPageObject(this.driver);
+
+        WebElement searchInit = mainPO.waitForElementPresent(
+                "id:org.wikipedia:id/search_container",
+                "Cannot find Search Wikipedia init search input"
+        );
+        searchInit.click();
+
+        WebElement searchInput = mainPO.waitForElementPresent(
+                "id:org.wikipedia:id/search_src_text",
+                "Cannot find search input"
+        );
+        searchInput.sendKeys("Java");
+
+        WebElement expectedResult = mainPO.waitForElementPresent(
+                "xpath://*[./*[contains(@text,'Island of Indonesia')]] ",
+                "Cannot find result 'Island of Indonesia' on 'Java' search"
+        );
+        expectedResult.click();
+
+       /*WebElement searchInputs = mainPO.waitForElementPresent(
+                "id:org.wikipedia:id/search_src_text",
+                "Cannot find search input"
+        );
+        searchInput.sendKeys("wfewfewfwegweg");
+
+
+*/
+
+    }
+}
